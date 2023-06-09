dbe69db706b198afb99ed129d5486e941ee140ae
marcin-filipiak <m.filipiak@noweenergie.org>
2023-06-09 14:28:32 +0200

    first push

--- main.cpp
+++ main.cpp
@@ -7,21 +7,41 @@ int main()
 {
 
 
-    float delta, x;
+    float delta, x, farea;
      float a,b,c,d;
      float sum = 0;
 
-     cout<<"pipe spacing:\n";
+     cout<<"\n pipe spacing [cm]:";
      cin>>delta;
 
-     cout<<"lenght wall A:\n";
+     cout<<"\n lenght wall A [cm]:";
      cin>>a;
-     cout<<"lenght wall B:\n";
+     cout<<"\n lenght wall B [cm]:";
      cin>>b;
-     cout<<"wall C=A\n";
-     c=a;
-     cout<<"wall D=B (the pipes enter the room by wall D)\n";
-     d=b;
+
+     c=a;//side c=a
+     d=b;//side d=b
+
+
+     cout<<"\n============================\n";
+
+     //floading area
+     farea = (a*b)/10000;
+     cout<<"floading area: "<<farea<<" [m2]\n";
+     if (farea>39) {
+         cout<<"ERROR! Floading area is too big! Should be less than 40m2\n";
+         exit(0);
+     }
+
+     //the ratio of the lengths of the sides of the plate
+     if (a/b >=2){
+         cout<<"ERROR! The ratio of lenghts sides should be <2, now a/b: "<<a/b<<"\n";
+         exit(0);
+     }
+     if (b/a >=2){
+         cout<<"ERROR! The ratio of lenghts sides should be <2, now b/a: "<<b/a<<"\n";
+         exit(0);
+     }
 
      //wall A
      x=0;
@@ -59,9 +79,13 @@ int main()
          sum = sum + x;
      }while(x-delta>0);
 
+     cout <<"\nPipe lenght: "<<sum/100<<" [m]\n";
 
-     cout <<"\n\nPipe lenght: "<<sum<<"\n\n";
+     if (sum/100 > 100) {
+         cout<<"ERROR! Pipe lenght is too long, should be less than 100m\n";
+         exit(0);
+     }
 
-
+    cout<<"\n\n";
     return 0;
 }
